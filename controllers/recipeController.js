5ee8616bda7619165c530753b955e2422f445a20
לאה זאבי <LeaZe@smg.local>
2025-03-10 14:06:39 +0200

    deleteRecipe

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,5 +1,18 @@
 const Recipe = require('../models/Recipe')
-exports.removeRecipe=async(reqj,res)=>{};
+exports.removeRecipe=async(reqj,res)=>{
+  const  RecipeId  = req.params._id;
+  console.log(RecipeId);
+    try {
+      const deleteRecipe = await Recipe.findOneAndDelete({ id: _id });
+      if (!deletedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+      console.error('Failed to delete user:', error);
+      res.status(500).json({ message: 'Failed to delete user' });
+    }
+};
 exports.updateRecipe=async(reqj,res)=>{};
 
 exports.getAllRecpies = async (req, res) => {
@@ -15,3 +28,8 @@ exports.addRecipe = async(req,res)=>{
   const recipe = await Recipe.create(req.body);
   res.json(recipe)
 };
+
+exports.deleteRecipe= async (req, res) => {
+
+};
+
